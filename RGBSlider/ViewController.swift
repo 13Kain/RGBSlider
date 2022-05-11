88344ca4eae492a0d55a844f13c2c13336b34ac8
Nikita Yashin <n.yashin88@icloud.com>
2022-05-11 16:44:31 +0300

    my final release

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -20,11 +20,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rgbView.layer.cornerRadius = 15
         redValue.text = String(format: "%.2f", redSlider.value)
         greenValue.text = String(format: "%.2f", greenSlider.value)
         blueValue.text = String(format: "%.2f", blueSlider.value)
     }
     
+
+    @IBAction func redSliderChange() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+    }
+    @IBAction func greenSliderChange() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+    }
+    @IBAction func blueSliderChange() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
+     
+    
+    @IBAction func rgbViewChange(_ sender: UISlider) {
+        rgbView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
     
 }
 
